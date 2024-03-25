6235d5304c873e6912a36ea0741cf9d39ba1c5e3
Harihek <145952940+Harihek@users.noreply.github.com>
2024-03-25 10:18:27 +0700

    project
    context

---
+++ HR_project/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Global_ASAX
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // BeginRequest += MvcApplication_BeginRequest;
+        }
+        private void Application_BeginRequest(object sender, EventArgs e)
+        {
+            var app = (HttpApplication)sender;
+            var uriObject = app.Context.Request.Url;
+            var absolute_path = uriObject.AbsolutePath;
+            if(HttpContext.Current.Session != null)
+            {
+                if (absolute_path != null)
+                {
+                    //check acc go to absolute_path
+                }
+            }
+            else
+            {
+                //check enable|disable redirect tp action
+                //HttpContext.Current.RewritePath("Home/About");
+            }
+        }
+    }
+}
